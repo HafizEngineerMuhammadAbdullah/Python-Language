f1ba198f1463aebe1511e60fb829641cb604cbf4
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-08-04 12:13:43 -0700

    Initiating Loop concepts in Python

---
+++ Practice-Python/Loops_In_Python.py
@@ -0,0 +1,24 @@
+# Loops in Python
+# Loops are used to perform repeating works(repeat instructions)
+# While Loop:-
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+count = 1
+while count <= 5:
+    print("Hello World!")
+    count += 1
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
+print("Loop Ended")
