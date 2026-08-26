d2a2159c90399330c8af57b612db7ab4ca52cbea
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-08-26 10:25:59 -0700

    Learning Functions in Python

---
+++ Practice-Python/Functions_In_Python.py
@@ -0,0 +1,24 @@
+a = 10
+b = 30
+sum = a + b
+print(sum)
+
+# more lines of code
+
+a = 24
+b = 35
+sum = a + b
+print(sum)
+
+# more lines of code
+
+a = 34
+b = 56
+sum = a + b
+print(sum)
+
+# this is the etire function to calculate the sum of two numbers
+def calc_sum(a,b):
+    sum = a + b
+    print(sum)
+    return sum
