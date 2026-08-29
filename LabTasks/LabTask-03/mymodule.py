07f4cb12339460d95c8c2b18cc43615bdd82c1fc
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-08-29 10:48:48 -0700

    Lab-03 Completed

--- LabTasks/LabTask-03/mymodule.py
+++ LabTasks/LabTask-03/mymodule.py
@@ -2,7 +2,9 @@
 # from math import sqrt
 # import math.sqrt as squareroot
 from math import sqrt as squareroot 
+import glob
 import time
+import random
 # help(math)
 # help(math.sqrt)
 # print(sqrt(9))
@@ -16,7 +18,34 @@ print("Current time in miliseconds : ", current_time)
 print("current time : ",time.ctime(current_time))
 print("current time : ",time.ctime())
 
-print(time.sleep(10))
+print(time.sleep(4))
+
+
+print(glob.glob("*"))
+
+print(glob.glob("*.txt"))
+
+# generates a random  integer between a and b inclusive
+print(random.randint(1,5))
+
+# generates a random  inumber between 0 and 1
+print(random.random())
+
+list = [1,2,34,56,78]
+random.shuffle(list)
+print(list)
+
+
+# random.shuffle(list,1)
+# print(x)
+my_list = [1, 2, 3, 4, 5]
+random.shuffle(my_list)
+print(my_list)
+
+
+
+
+
 
 
 
