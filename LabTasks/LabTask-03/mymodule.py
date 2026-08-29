ebd35478bc7ea65d21495a74de62bacc5a16c251
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-08-28 22:28:49 -0700

    Doing Lab-03

--- LabTasks/LabTask-03/mymodule.py
+++ LabTasks/LabTask-03/mymodule.py
@@ -1,7 +1,24 @@
 # import math
-from math import sqrt
+# from math import sqrt
+# import math.sqrt as squareroot
+from math import sqrt as squareroot 
+import time
 # help(math)
 # help(math.sqrt)
-print(sqrt(9))
+# print(sqrt(9))
+print("Square root of 81 is", squareroot(81))
 def greeting(name):
    print("Hello,Welcome to " , name)
+
+current_time = time.time()
+print("Current time in miliseconds : ", current_time)
+
+print("current time : ",time.ctime(current_time))
+print("current time : ",time.ctime())
+
+print(time.sleep(10))
+
+
+
+
+
